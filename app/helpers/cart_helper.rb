cb45efc64b957506d372d3f0c68ededaf5d151d1
Bri Dotson <bridotson@gmail.com>
2014-12-18 11:27:36 -0800

    add shipping quote method to helper, add shipping query class

--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -1,2 +1,9 @@
 module CartHelper
+  def show_shipping_options(address, items)
+    weights = items.collect{|item| item.product.weight.to_i}
+    city = address.city
+    state = address.state
+    quotes = ShippingQuery.new(weights, city, state)
+    
+  end
 end
---
+++ app/queries/shipping_query.rb
@@ -0,0 +1,15 @@
+class ShippingQuery
+  def initialize(weights,city,state)
+    @weights=weights
+    @city=city
+    @state=state
+  end
+
+  def ups_quote
+    
+  end
+
+  def usps_quote
+
+  end
+end
