5bc0e80ed0e362d29a7ad29fcc9ec3ba3d486839
Bri Dotson <bridotson@gmail.com>
2014-12-19 15:15:15 -0800

    make dimensions into strings so it works with the api

--- app/queries/shipping_query.rb
+++ app/queries/shipping_query.rb
@@ -1,9 +1,9 @@
 class ShippingQuery
   def initialize(weights,city,state, postal_code)
-    @query = query(weights, city, state, postal_code)
     @shipping_api = "http://ship-my-ducks.herokuapp.com/"
     # make an array of weights.length, each element is [20,20,20]
-    @dimensions = weights.length.times.collect{[20,20,20]}
+    @dimensions = weights.length.times.collect{["20","20","20"]}
+    @query = query(weights, city, state, postal_code)
   end
 
   def query(weights, city, state, postal_code)
