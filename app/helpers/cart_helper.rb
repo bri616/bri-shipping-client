89419795bae09425b229acff834205691c788818
Bri Dotson <bridotson@gmail.com>
2014-12-18 16:42:50 -0800

    choose the cheapest three shipping options from each provider

--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -6,14 +6,20 @@ module CartHelper
     state = address.state
     postal_code = address.postal_code
     quotes = ShippingQuery.new(weights, city, state, postal_code)
-    allquotes = quotes.ups_quote.merge(quotes.usps_quote)
+    upsquotes = quotes.ups_quote
+    uspsquotes = quotes.usps_quote
     # make an array of ShippingOption objects
 
-    allquotes.collect! do |option_type, option_price|
+    upsquotes = upsquotes.collect do |option_type, option_price|
       ShippingOption.new(shipping_type: option_type, price: option_price.to_i)
     end
 
-    
+    uspsquotes = uspsquotes.collect do |option_type, option_price|
+      ShippingOption.new(shipping_type: option_type, price: option_price.to_i)
+    end
+
+    upsquotes.sort_by(&:price).first(3) + uspsquotes.sort_by(&:price).first(3)
+
   end
 
   def shipping_option_label(shipping_option)
